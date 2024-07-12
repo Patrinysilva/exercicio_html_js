62868f57bc8d4a85d02c7b2691a93d913266aead
Patrinysilva <patrinysilva17@gmail.com>
2024-07-12 16:55:41 -0300

    finalizando o exercicio

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 document.getElementById('formulario').addEventListener('submit', function(event) {
     event.preventDefault();
 
-
-    const primeiroNumero = document.getElementById('campoA').value;
-    const segundoNumero = document.getElementById('campoB').value;
+    const primeiroNumero = parseFloat(document.getElementById('campoA').value);
+    const segundoNumero = parseFloat(document.getElementById('campoB').value);
     const mensagem = document.getElementById('mensagem');
 
-    if (campoB > campoA) {
+    if (segundoNumero > primeiroNumero) {
         mensagem.textContent = "O número B é maior que o número A.";
         mensagem.className = "message positive";
     } else {
-        mensagem.textContent = "O número A deve ser maior que o número B.";
+        mensagem.textContent = "O número A é maior que o número B.";
         mensagem.className = "message negative";
     }
 
     mensagem.style.display = "block";
-
-})
+});
